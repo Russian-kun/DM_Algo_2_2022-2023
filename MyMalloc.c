74d9dad52baa801a5fa79ebe0e3121ceeae3caf8
Russian-kun <axel.baillargeau@gmail.com>
2023-03-28 11:45:11 +0200

    malloc

--- MyMalloc.c
+++ MyMalloc.c
@@ -1,13 +1,65 @@
 #include "MyMalloc.h"
 
+#include "MyMalloc.h"
+#include <stdint.h>
+
 #define POOL_SIZE 1000000000 /* 1 Giga */
+#define ALIGNMENT 8
+#define ALIGN(size) (((size) + (ALIGNMENT - 1)) & ~(ALIGNMENT - 1))
+#define HEADER_SIZE (sizeof(size_t))
 
 static char _mem_pool[POOL_SIZE]; /* static pool of memory */
+static block_t* free_list = NULL;
 
-void* my_malloc(size_t size) {
-    /* Bon courage !!!! */
+static void* allocate_block(size_t size) {
+    void* ptr = NULL;
+    block_t* block = free_list;
+    block_t** indirect = &free_list;
+
+    while (block != NULL) {
+        if (block->size >= size) {
+            /* Found a block that is large enough */
+            size_t remainder = block->size - size;
+
+            if (remainder >= sizeof(block_t) + ALIGNMENT) {
+                /* Split the block and create a new one */
+                block_t* new_block = (block_t*)((uintptr_t)block + HEADER_SIZE + size);
+                new_block->size = remainder - HEADER_SIZE;
+                new_block->next = block->next;
+                *indirect = new_block;
+                ptr = (void*)((uintptr_t)block + HEADER_SIZE);
+                block->size = size;
+                return ptr;
+            } else {
+                /* Use the entire block */
+                *indirect = block->next;
+                ptr = (void*)((uintptr_t)block + HEADER_SIZE);
+                return ptr;
+            }
+        }
+
+        indirect = &block->next;
+        block = block->next;
+    }
+
+    return NULL;
 }
 
-void my_free(void* pouet) {
-    /* Bon courage !!!! */
+void my_malloc_init(void) {
+    block_t* block = (block_t*)_mem_pool;
+    block->size = POOL_SIZE - HEADER_SIZE;
+    block->next = NULL;
+    free_list = block;
+}
+
+void* my_malloc(size_t size) {
+    size_t alloc_size = ALIGN(size + HEADER_SIZE);
+    void* ptr = allocate_block(alloc_size);
+
+    if (ptr == NULL) {
+        return NULL;
+    }
+
+    *(size_t*)((uintptr_t)ptr - HEADER_SIZE) = alloc_size;
+    return ptr;
 }
