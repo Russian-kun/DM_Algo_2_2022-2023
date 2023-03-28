e878e4b17c9e26e94c6b3851ddef630186fcc097
Russian-kun <axel.baillargeau@gmail.com>
2023-03-28 11:45:37 +0200

    header

--- MyMalloc.h
+++ MyMalloc.h
@@ -3,6 +3,13 @@
 
 #include <stdlib.h>
 
+typedef struct block_t {
+    size_t size;
+    struct block_t* next;
+} block_t;
+
+void my_malloc_init(void);
+
 void* my_malloc(size_t size);
 
 void my_free(void* pouet);
