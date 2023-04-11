55eecb39a7907f24895f4e7de129d40c94f7372e
Russian-kun <axel.baillargeau@gmail.com>
2023-04-11 17:31:24 +0200

    main prgm

--- main.c
+++ main.c
@@ -1,9 +1,31 @@
 #include "Quadtree.h"
+#include "affichage.h"
+#include <time.h>
 
-int main(int argc, char** argv) {
-    Quadtree qd = initQuadtree(100, 10);
-    Particle* p = generateParticles(100);
+int main(int argc, char* argv[]) {
+    int W = 512, wmin = 10, kp = 100;
+    int nbp = 500;
+    Cell* lstCell = NULL;
+    srand(time(NULL));
+    Quadtree qd = initQuadtree(W, wmin);
+    Particle* p = generateParticles(nbp, &lstCell);
+    addParticlesQuadtree(qd, p, lstCell, nbp, kp);
+
+    MLV_create_window("Quadtree", "Quadtree", W, W);
+    MLV_clear_window(MLV_COLOR_BLACK);
+    for (int i = 0; i < nbp; i++) {
+        afficherParticle(&p[i]);
+    }
+    afficherQuadtree(qd, W, wmin);
+
+    MLV_actualise_window();
+    MLV_update_window();
+    MLV_wait_event(NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+    MLV_free_window();
+
     freeQuadtree(qd);
     free(p);
+    free(lstCell);
     return 0;
 }
