c79f00bd7cf58537ac14134bcce31d9e6c5793dd
Russian-kun <axel.baillargeau@gmail.com>
2023-04-11 17:31:08 +0200

    affichage

--- affichage.c
+++ affichage.c
@@ -1,1 +1,23 @@
 #include "affichage.h"
+
+void afficherQuadtreeRec(Quadtree qt, int W, int wmin) {
+    if (qt == NULL) return;
+    if (qt->height < wmin) {
+        MLV_draw_rectangle(qt->x, qt->y, qt->height, qt->height, MLV_COLOR_RED);
+        return;
+    }
+    if (qt->nbp != 0)
+        MLV_draw_rectangle(qt->x, qt->y, qt->height, qt->height, MLV_COLOR_WHITE);
+    afficherQuadtreeRec(qt->nw, W, wmin);
+    afficherQuadtreeRec(qt->ne, W, wmin);
+    afficherQuadtreeRec(qt->sw, W, wmin);
+    afficherQuadtreeRec(qt->se, W, wmin);
+}
+
+void afficherQuadtree(Quadtree qt, int W, int wmin) {
+    afficherQuadtreeRec(qt, W, wmin);
+}
+
+void afficherParticle(Particle* p) {
+    MLV_draw_filled_circle(p->x, p->y, 1, MLV_COLOR_BLUE);
+}
--- affichage.h
+++ affichage.h
@@ -1,1 +1,11 @@
+#ifndef AFFICHAGE_H
+#define AFFICHAGE_H
+
 #include "MLV/MLV_all.h"
+#include "Quadtree.h"
+
+void afficherQuadtree(Quadtree qt, int W, int wmin);
+
+void afficherParticle(Particle* p);
+
+#endif
