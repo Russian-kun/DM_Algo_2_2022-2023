b652948efb2a2b8ec5e108fa2e68c91a4ff7d536
Russian-kun <axel.baillargeau@gmail.com>
2023-04-12 19:24:10 +0200

    prettier

--- affichage.c
+++ affichage.c
@@ -9,11 +9,12 @@ void afficherQuadtreeRec(Quadtree qt, int W, int wmin) {
     }
     if (qt->plist != NULL) {
         MLV_draw_rectangle(qt->x, qt->y, qt->height, qt->height, MLV_COLOR_WHITE);
+        MLV_draw_filled_rectangle(qt->x, qt->y, qt->height, qt->height, MLV_rgba(100 + 12 * qt->nbp, 50, 50 + 15 * qt->nbp, 40));
         // MLV_actualise_window();
         if (qt->nbp > 10) {
             fprintf(stderr, "\nErreur: Tentative d'affichage d'un noeud avec plus de 10 particules\nnbp: %d", qt->nbp);
         }
-        // return;
+        return;
     }
     afficherQuadtreeRec(qt->nw, W, wmin);
     afficherQuadtreeRec(qt->ne, W, wmin);
